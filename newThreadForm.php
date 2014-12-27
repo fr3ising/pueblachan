eb6b503e9e3beaf8ec6e67ab10dc3c3ad569a074
Alfonso Egio <alfonso.egio@gmail.com>
2014-12-27 19:03:25 +0100

    Discusiones fuera

--- newThreadForm.php
+++ newThreadForm.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-	<title>Crear nuevo tema - PuEblALeAKs</title>
+	<title>Crear nuevo tema - PuEblAchAn</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 	<meta charset="iso-8859-1"/>
--- thread.php
+++ thread.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-	<title>PuEblALeAKs</title>
+	<title>PuEblAchAn</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 	<meta charset="iso-8859-1"/>
